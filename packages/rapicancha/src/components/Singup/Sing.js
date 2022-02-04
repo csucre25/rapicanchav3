28455c8906c9cc671bb3cc155742890010df4f80
MariT29fsh <161764@unsaac.edu.pe>
2022-02-03 21:48:51 -0500

    create component Sing.js

--- packages/rapicancha/src/components/Singup/Sing.js
+++ packages/rapicancha/src/components/Singup/Sing.js
@@ -4,12 +4,11 @@ import './sing.scss';
 
 
 class Sing extends React.Component {
-    
+
     render() {
-        
+
         return (
-                <div className="login-form">
-                    
+            <div className="login-form">
                 <div className="login-header">
                     <h1>Registrarse</h1>
                 </div>
@@ -17,21 +16,29 @@ class Sing extends React.Component {
                     <img className="logo1" src={"https://images.vexels.com/media/users/3/137253/isolated/lists/90dd9f12fdd1eefb8c8976903944c026-logotipo-del-icono-de-facebook.png"} alt="50px"
                         height="50px"></img>
                     <img src={"https://www.doplerweb.com/data/fotos/1022008841_1_twitter-bird-white-on-blue1.jpg"} alt="50" width="50"></img>
-                </div><br/>
+                </div>
+                <br />
                 <h2 class="forgot-password">o registrese con tu email</h2>
-                <h3 class="derecha_info">Correo electronico</h3>
-                <input type="text" placeholder="example@gmail.com" /><br /><br/>
-                <h3 class="derecha_info">Nombre</h3>
-                <input type="text" placeholder="Juan Antonio" /><br /><br/>
-                <h3 class="derecha_info">Apellidos</h3>
-                <input type="text" placeholder="Del Solar" /><br /><br/>
-                <h3 class="derecha_info">Nro de Celular</h3>
-                <input type="text" placeholder="999 999 999" /><br /><br/>
-                <h3 class="derecha_info">Contraseña</h3>
-                <input type="password" placeholder="Contraseña" />
-                <br/><br/>
-                <h3 class="derecha_info">Repita contraseña</h3>
-                <input type="text" placeholder="contraseña" /><br /><br/>
+                <div className="info-usuario">
+                    <div >
+                        <h3 class="derecha_info">Correo electronico</h3><br />
+                        <h3 class="derecha_info">Nombre</h3><br />
+                        <h3 class="derecha_info">Apellidos</h3><br />
+                        <h3 class="derecha_info">Nro de Celular</h3><br />
+                        <h3 class="derecha_info">Contraseña</h3><br />
+                        <h3 class="derecha_info">Repita contraseña</h3>
+                    </div>
+                    <div>
+                        <input type="text" placeholder="example@gmail.com" /><br/>
+                        <input type="text" placeholder="Juan Antonio" /><br />
+                        <input type="text" placeholder="Del Solar" /><br />
+                        <input type="text" placeholder="999 999 999" /><br />
+                        <input type="password" placeholder="Contraseña" />
+                        <br />
+                        <input type="text" placeholder="Contraseña" /><br />
+                    </div>
+
+                </div>
                 <input type="button" value="Registrarse" class="registrase-button" />
                 <br />
                 <h4 class="no-access">¿Ya tiene una cuenta?</h4>
@@ -39,7 +46,7 @@ class Sing extends React.Component {
                 <br />
 
             </div>
-            
+
 
         );
     }
